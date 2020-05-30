2892debde455a5b0e7a14f4ff6d73cb1de3e0523
Mateus Pestana <matheus.pestana@iesp.uerj.br>
2020-05-30 19:22:57 -0300

    Adaptando desafio "ViewAndModifiers"

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,71 +8,86 @@
 
 import SwiftUI
 
+struct RedMod: ViewModifier {
+	func body(content: Content) -> some View {
+		content
+			.foregroundColor(.red)
+	}
+}
 
+extension View {
+	func redModifier() -> some View {
+		self.modifier(RedMod())
+	}
+}
 
 struct ContentView: View {
-    
-    
-    @State private var valorConta = ""
-    @State private var numeroDePessoas = 2
-    @State private var pctGorjeta = 2
-    
-    let pctGorjetas = [10, 15, 20, 25, 0]
-    
-    var totalPorPessoa: Double {
-        let contagemPessoas = Double(numeroDePessoas + 2)
-        let selecaoGorjeta = Double(pctGorjetas[pctGorjeta])
-        let ordemValor = Double(valorConta) ?? 0
-        
-        let valorGorjeta = ordemValor / 100  * selecaoGorjeta
-        let grandTotal = ordemValor + valorGorjeta
-        let totalPorPessoa = grandTotal / contagemPessoas
-        
-        return totalPorPessoa
-    }
-    
-    var body: some View {
-        NavigationView{
-            Form {
-                Section {
-                    TextField("Valor", text: $valorConta)
-                        .keyboardType(.decimalPad)
-                    
-                    Picker("Número de pessoas", selection: $numeroDePessoas) {
-                        ForEach(2..<100) {
-                            Text("\($0) pessoas")
-                        }
-                    }
-                    
-                }
-                
-                Section(header: Text("Quanto de gorjeta vocês querem deixar?")) {
-                    Picker("Gorjeta", selection: $pctGorjeta) {
-                        ForEach(0 ..< pctGorjetas.count) {
-                            Text("\(self.pctGorjetas[$0])%")
-                        }
-                        .pickerStyle(SegmentedPickerStyle())
-                    }
-                }
-                
-                Section  {
-                    
-                    Text("R$ \(totalPorPessoa, specifier: "%.2f")")
-                }
-            }
-            .navigationBarTitle("WeSplit")
-        }
-        
-        
-        
-    }
+	
+	
+	@State private var valorConta = ""
+	@State private var numeroDePessoas = 2
+	@State private var pctGorjeta = 1
+	
+	let pctGorjetas = [0, 10, 12, 15, 20, 25]
+	
+	var totalPorPessoa: Double {
+		let contagemPessoas = Double(numeroDePessoas + 2)
+		let selecaoGorjeta = Double(pctGorjetas[pctGorjeta])
+		let ordemValor = Double(valorConta) ?? 0
+		
+		let valorGorjeta = ordemValor / 100  * selecaoGorjeta
+		let grandTotal = ordemValor + valorGorjeta
+		let totalPorPessoa = grandTotal / contagemPessoas
+		
+		return totalPorPessoa
+	}
+	
+	var body: some View {
+		NavigationView{
+			Form {
+				Section {
+					TextField("Valor", text: $valorConta)
+						.keyboardType(.decimalPad)
+					
+					Picker("Número de pessoas", selection: $numeroDePessoas) {
+						ForEach(2..<100) {
+							Text("\($0) pessoas")
+						}
+					}
+					
+				}
+				
+				Section(header: Text("Quanto de gorjeta vocês querem deixar?")) {
+					Picker("Gorjeta", selection: $pctGorjeta) {
+						ForEach(0 ..< pctGorjetas.count) {
+							Text("\(self.pctGorjetas[$0])%")
+						}
+						.pickerStyle(SegmentedPickerStyle())
+					}
+				}
+				
+				Section  {
+					if pctGorjeta != 0 {
+					Text("R$ \(totalPorPessoa, specifier: "%.2f")")
+					} else {
+						Text("R$ \(totalPorPessoa, specifier: "%.2f")").redModifier()
+					}
+//					Text("R$ \(totalPorPessoa, specifier: "%.2f")")
+				}
+			}
+			.navigationBarTitle("WeSplit")
+		}
+		
+		
+		
+	}
 }
 
 
 
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
